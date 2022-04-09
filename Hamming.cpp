db4f78b59701b6234967b989c9b2311f8d75e681
Igor Braga Palhano <94334598+BPalhano@users.noreply.github.com>
2022-04-08 21:24:38 -0300

    Update Hamming.cpp

--- Hamming.cpp
+++ Hamming.cpp
@@ -56,20 +56,11 @@ string Hamming(string x){
   }
 
   int pot[5] = {1,2,4,8,16};
-  int pot2[5] = {palavra[2] ^ palavra[4] ^ palavra[6] ^ palavra[8] ^ palavra[10] ^ palavra[12] ^ palavra[14],palavra[2] ^ palavra[5]^ palavra[6]^ palavra[9]^ palavra[10]^ palavra[13]^ palavra[14],palavra[4] ^ palavra[5] ^ palavra[6]^ palavra[11]^ palavra[12]^ palavra[13]^ palavra[14],palavra[8] ^ palavra[9]^ palavra[10]^ palavra[11]^ palavra[12]^ palavra[13]^ palavra[14],0};
   
-
-  x1 = palavra[2] ^ palavra[4] ^ palavra[6] ^ palavra[8] ^ palavra[10] ^ palavra[12] ^ palavra[14];
-
-  x2 = palavra[2] ^ palavra[5]^ palavra[6]^ palavra[9]^ palavra[10]^ palavra[13]^ palavra[14];
-
-  x4 = palavra[4] ^ palavra[5] ^ palavra[6]^ palavra[11]^ palavra[12]^ palavra[13]^ palavra[14];
-
-  x8 = palavra[8] ^ palavra[9]^ palavra[10]^ palavra[11]^ palavra[12]^ palavra[13]^ palavra[14];
-
-//x16 = ?????
+  int pot2[5] = {palavra[0] ^ palavra[1] ^ palavra[3] ^ palavra[4] ^ palavra[6] ^ palavra[8] ^ palavra[10] ^ palavra[11] ^ palavra[13] ^ palavra[15],palavra[0] ^ palavra[2] ^ palavra[3] ^ palavra[5] ^ palavra[6] ^ palavra[9] ^ palavra[10] ^ palavra[12] ^ palavra[13], palavra[1] ^ palavra[2] ^ palavra[3] ^ palavra[7] ^ palavra[8] ^ palavra[9] ^ palavra[10] ^ palavra[14] ^ palavra[15], palavra[4] ^ palavra[5] ^ palavra[6] ^ palavra[7] ^ palavra[8] ^ palavra[9] ^ palavra[10], palavra[11] ^ palavra[12] ^ palavra[13] ^ palavra[14] ^ palavra[15]};
+  
   int count = 0;
-  for(i = 0; i < 24; i++){
+  for(i = 0; i < 21; i++){
 
     if (exist_this_element(pot, i, 5)){
       hamming = hamming+(char(pot2[count]));
@@ -88,7 +79,7 @@ string Hamming(string x){
 
 int main (void){
 
-  // ler um inteiro de 32 bits.
+  // ler um inteiro de 16 bits.
   int read;
   cin >> read;
   
@@ -96,6 +87,6 @@ int main (void){
   string x = bitset<16>(read).to_string();
   string H = Hamming(x);
 
-  cout << H;
+  cout << "código com redundância:" << H << endl << "código original: " << x;
 }
 
