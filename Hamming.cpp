eee7802b982301ae4118771f7749bb6612dd28c5
Igor Braga Palhano <94334598+BPalhano@users.noreply.github.com>
2022-05-03 13:12:33 -0300

    Update Hamming.cpp

--- Hamming.cpp
+++ Hamming.cpp
@@ -52,30 +52,51 @@ string Hamming(string x){
 
   for(i = 0; i < 16; i++){
 
-    palavra[i] = int(x[i]);
+    palavra[i] = int(x[i]) - 48;
   }
 
   int pot[5] = {1,2,4,8,16};
   
-  int pot2[5] = {palavra[0] ^ palavra[1] ^ palavra[3] ^ palavra[4] ^ palavra[6] ^ palavra[8] ^ palavra[10] ^ palavra[11] ^ palavra[13] ^ palavra[15],palavra[0] ^ palavra[2] ^ palavra[3] ^ palavra[5] ^ palavra[6] ^ palavra[9] ^ palavra[10] ^ palavra[12] ^ palavra[13], palavra[1] ^ palavra[2] ^ palavra[3] ^ palavra[7] ^ palavra[8] ^ palavra[9] ^ palavra[10] ^ palavra[14] ^ palavra[15], palavra[4] ^ palavra[5] ^ palavra[6] ^ palavra[7] ^ palavra[8] ^ palavra[9] ^ palavra[10], palavra[11] ^ palavra[12] ^ palavra[13] ^ palavra[14] ^ palavra[15]};
+  int pot2[5] = 
+
+{palavra[0] ^ palavra[1] ^ palavra[3] ^ palavra[4] ^ palavra[6] ^ palavra[8] ^ palavra[10] ^ palavra[11] ^ palavra[13] ^ palavra[15],
+  palavra[0] ^ palavra[2] ^ palavra[3] ^ palavra[5] ^ palavra[6] ^ palavra[9] ^ palavra[10] ^ palavra[12] ^ palavra[13],
+  palavra[1] ^ palavra[2] ^ palavra[3] ^ palavra[7] ^ palavra[8] ^ palavra[9] ^ palavra[10] ^ palavra[14] ^ palavra[15],
+  palavra[4] ^ palavra[5] ^ palavra[6] ^ palavra[7] ^ palavra[8] ^ palavra[9] ^ palavra[10],
+  palavra[11] ^ palavra[12] ^ palavra[13] ^ palavra[14] ^ palavra[15]};
   
   int count = 0;
   for(i = 0; i < 21; i++){
 
     if (exist_this_element(pot, i, 5)){
-      hamming = hamming+(char(pot2[count]));
+      
+      hamming += (to_string(pot2[count]));
 
         count++;
     }
     else{
 
-      hamming = hamming+(char(palavra[i]));
+      hamming += (to_string(palavra[i]));
+      
     }
   }
 
+  for( i =0; i < 5;i++){
+    
+    cout <<"pot2"<< i <<endl << pot2[i] << endl;
 
+  }
+
+  for(i = 0; i < 16; i++){
+  
+  cout << "palavra: "<< i << endl << palavra[i] << "x" << x[i] << endl;
+}
+
+  
   return hamming;
 }
+
+
 
 int main (void){
 
@@ -87,6 +108,6 @@ int main (void){
   string x = bitset<16>(read).to_string();
   string H = Hamming(x);
 
-  cout << "código com redundância:" << H << endl << "código original: " << x;
+  cout << "código com redundância:" << H << " num de char: " << H.size() << endl << "código original: " << x << " num de char: " << x.size();
 }
 
