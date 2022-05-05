4b93a685ec96e70502f43243cbb516a487f1c2f0
Igor Braga Palhano <94334598+BPalhano@users.noreply.github.com>
2022-05-05 13:01:37 -0300

    Update Hamming.cpp

--- Hamming.cpp
+++ Hamming.cpp
@@ -2,44 +2,6 @@
 #include <string>
 using namespace std;
 
-bool exist_this_element(int *vetor, int number, int array_size){
-
-  int i;
-  for (i = 0; i < array_size; i++){
-
-    if(vetor[i] == number){
-      return true;
-    }
-  }
-
-  return false;
-}
-
-int pot2finder(int number){
-
-  int count = 0;
-
-  while(number != 0){
-
-    if (count == 0 && number == 1 || count == 0 & number == 0){
-      return 0;
-    }
-      
-    else{
-      number = floor(number/2);
-    
-      if (number > 1){
-      
-        count++;
-      }
-    }
-  }
-  count++;
-
-  return count;
-  
-}
-
 string Hamming(string x){
 
 
@@ -54,8 +16,9 @@ string Hamming(string x){
 
     palavra[i] = int(x[i]) - 48;
   }
-
-  int pot[5] = {0,1,3,7,15};
+  
+  bool bitcheck[21]={true,true,false,true,false,false,false,true,false,false,false,false,
+  false,false,false,true,false,false,false,false,false};
   
   int pot2[5] = 
 
@@ -66,32 +29,26 @@ string Hamming(string x){
   palavra[11] ^ palavra[12] ^ palavra[13] ^ palavra[14] ^ palavra[15]};
   
   int count = 0;
+  int count_aux = 0;
   
-  for(i = 0; i < 21; i++){
+  
+  for (i = 0; i < 21; i++){
 
-    if (exist_this_element(pot, i, 5)){
+    if (bitcheck[i]){
       
       hamming += (to_string(pot2[count]));
-
         count++;
     }
     else{
 
-      hamming += (to_string(palavra[i]));
+      hamming += (to_string(palavra[count_aux]));
+      count_aux++;
       
     }
   }
 
-  for( i =0; i < 5;i++){
-    
-    cout <<"pot2 "<< i <<endl << pot2[i] << endl;
-
-  }
-  
   return hamming;
 }
-
-
 
 int main (void){
 
@@ -99,10 +56,12 @@ int main (void){
   int read;
   cin >> read;
   
-
   string x = bitset<16>(read).to_string();
   string H = Hamming(x);
 
   cout << "código com redundância:" << H << " num de char: " << H.size() << endl << "código original: " << x << " num de char: " << x.size();
+
+
+  return 0;
 }
 
